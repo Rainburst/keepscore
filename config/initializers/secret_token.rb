69da1ff9955deb6c118a204ecad4fcc3765d9ac3
Nikita Ilyasov <rainburst@gmail.com>
2014-03-04 23:01:25 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Keepscore::Application.config.secret_key_base = '7f3a5512c6e240bb684323fcb44c0b5f9f1bd0a9db4c944d887269478988f2ca3b446ba47b1bcdb6e9df63030cdd25be7fd1269e999515de9aabedaa09e3ff34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Keepscore::Application.config.session_store :cookie_store, key: '_keepscore_session'
